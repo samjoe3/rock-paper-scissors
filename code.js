50ecaf88e09aebcc83ed67cb0ab412ee418a13fa
samjoe3 <tylerstanco@hotmail.com>
2022-08-10 13:18:40 -0400

    added resultDisplay function that displays win lose or tie at the end of each game

--- code.js
+++ code.js
@@ -1,25 +1,34 @@
 function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1;
 }
-
+let result = "";
 function playRound(choice) {
     let userChoice = choice;
     let compChoice = getComputerChoice();
     if (userChoice === compChoice) {
+        console.log(userChoice, compChoice);
+        result = "You Tied. :|";
+        resultDisplay();
         return "tie";
     } 
-    else if ((userChoice === 1 && compChoice === 2)||(userChoice === 2 && compChoice === 1)||(userChoice === 3 && compChoice === 2)) {
+    else if ((userChoice === 1 && compChoice === 2)||(userChoice === 2 && compChoice === 3)||(userChoice === 3 && compChoice === 1)) {
+        console.log(userChoice, compChoice);
+        result = "You Lost. :(";
+        resultDisplay();
         return "lose";
     }
-    else if ((userChoice === 1 && compChoice === 3)||(userChoice === 2 && compChoice === 3)||(userChoice === 3 && compChoice === 1)) {
+    else if ((userChoice === 1 && compChoice === 3)||(userChoice === 2 && compChoice === 1)||(userChoice === 3 && compChoice === 2)) {
+        console.log(userChoice, compChoice);
+        result = "You Won! :)";
+        resultDisplay();
         return "win";
     }
-    else return "invalid";
 }
+let roundTies = 0;
+let roundWins = 0;
+let roundLosses = 0;
+
 function game(choice) {
-    let roundTies = 0;
-    let roundWins = 0;
-    let roundLosses = 0;
     //for (let i = 0; i < 5; i++) {
         let roundResult = playRound(choice);
 
@@ -35,11 +44,16 @@ function game(choice) {
             roundWins++;
             console.log(`Wins: ${roundWins}, Losses: ${roundLosses}, Ties: ${roundTies}.`);
         }
-        else if (roundResult === "invalid") {
-            i--;
-            console.log("Invalid answer, please try again.");
-        }
+
+        scoreBoard();
     }
+    function scoreBoard() {
+        document.getElementById("underboard").innerHTML = `Wins: ${roundWins}, Losses: ${roundLosses}, Ties: ${roundTies}.`;
+    }
+    function resultDisplay() {
+        document.getElementById("resultdisplay").innerHTML = result;
+    }
+
 //}
 //game();
 document.getElementById("rock").onclick = function() {game(1)};
